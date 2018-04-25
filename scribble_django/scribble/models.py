a744cf2e58f5412c9daf24e71725099e7b72ef7f
superbuggy <jpqreichard@gmail.com>
2018-04-25 12:55:13 -0400

    Add forgein key for artists on songs

--- scribble_django/scribble/models.py
+++ scribble_django/scribble/models.py
@@ -10,6 +10,7 @@ class Artist(models.Model):
         return self.name
 
 class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
     title = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
     preview_url = models.TextField()
