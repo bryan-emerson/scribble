d4768d62249042ae6cb5a96eb440092c0d06404b
superbuggy <jpqreichard@gmail.com>
2018-04-25 16:56:07 -0400

    Fix migrations

--- scribble_django/scribble/models.py
+++ scribble_django/scribble/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     nationality = models.CharField(max_length = 25)
-    photo_url = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Song(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs', null=True)
+    # pass
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
     title = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
-    preview_url = models.TextField()
+    preview_url = models.TextField(default="")
