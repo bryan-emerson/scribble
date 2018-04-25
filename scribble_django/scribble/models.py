eaa9d70153dd3d3de2ad3c05234da3acb2e108ed
superbuggy <jpqreichard@gmail.com>
2018-04-25 12:51:40 -0400

    Add model for Songs

--- scribble_django/scribble/models.py
+++ scribble_django/scribble/models.py
@@ -8,3 +8,8 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    preview_url = models.TextField()
