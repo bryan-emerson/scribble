699a94f41e15a932071bfb0d1f8a2f3b0d9f24b7
superbuggy <jpqreichard@gmail.com>
2018-04-26 11:30:13 -0400

    Add number of comments method for Post

--- scribble/models.py
+++ scribble/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100, default="")
-    author = models.CharField(max_length = 25, default="")
-    body = models.TextField(default="")
+    title = models.CharField(max_length=100, default='')
+    author = models.CharField(max_length = 25, default='')
+    body = models.TextField(default='')
 
     def __str__(self):
         return self.title
+    
+    def num_comments(self):
+        return len(list(self.comments.all()))
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.CharField(max_length = 25, default="")
-    body = models.TextField(default="")
+    author = models.CharField(max_length = 25, default='')
+    body = models.TextField(default='')
 
     def __str__(self):
-        return self.title
+        return f'{self.author} replies to {self.post.author} in {self.post.title}'
