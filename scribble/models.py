7cd4d23a93713abe878735eef2d04cd415d91a56
superbuggy <jpqreichard@gmail.com>
2018-04-25 20:16:45 -0400

    Migrate artists and songs

--- scribble/models.py
+++ scribble/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     nationality = models.CharField(max_length = 25)
+    photo_url = models.TextField(default="")
 
     def __str__(self):
         return self.name
 
 class Song(models.Model):
-    # pass
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
     title = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
