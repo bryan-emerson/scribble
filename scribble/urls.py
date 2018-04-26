7242cb60d4ca778dad8470f717749b24a12bdbf3
superbuggy <jpqreichard@gmail.com>
2018-04-26 11:47:05 -0400

    Add individual view and template for Post detail

--- scribble/urls.py
+++ scribble/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^posts$', views.post_list, name='post_list'),
+    url(r'^posts/(?P<primary_key>\d+)$', views.post_detail, name='post_detail')
 ]
--- scribble/views.py
+++ scribble/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     posts = list(Post.objects.all())
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, primary_key):
+    post = Post.objects.get(id=primary_key)
+    return render(request, 'post_detail.html', {'post': post})
